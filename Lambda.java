4371c488c97a48ec3adf225b5708cc6496ad4dc5
maiconmoretto <maiconmorettos@gmail.com>
2021-01-12 09:19:03 -0300

    Adicionado metodo impressor

--- Lambda.java
+++ Lambda.java
@@ -7,13 +7,15 @@ public class Lambda {
 
 	public static void main(String[] args) {
 		List<String> palavras = new ArrayList<String>();
-		palavras.add("casa");
+		palavras.add("casaaaaa");
 		palavras.add("apartamento");
 		palavras.add("sitio");
 
-		Collections.sort(palavras);
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
 		
-		palavras.forEach(s -> System.out.println(s));
+		//palavras.forEach(s -> System.out.println(s));
 
 	}
 }
